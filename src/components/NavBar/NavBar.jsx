9ba5464af2349755a1c457134eeecd54f07c82a4
kevin170703 <correakevinfabian01@gmail.com>
2023-01-08 13:00:32 -0300

    remaster navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,51 @@
 import React from "react";
-import s from "./NavBar.module.css"
+import s from "./NavBar.module.css";
 import { Link } from "react-scroll";
 
-
-export default function Nav(){
-  
- return(
+export default function Nav() {
+  return (
     <div className={s.nav}>
-    <Link activeClass="active" to="home" spy={true} smooth={true} offset={0} duration={500} className={s.links}>Inicio</Link>
-     <Link activeClass="active" to="sobremi" spy={true} smooth={true} offset={50} duration={500} className={s.links}>Sobre mi</Link>
-     <Link activeClass="active" to="proyectos" spy={true} smooth={true} offset={50} duration={500} className={s.links}>Proyectos</Link>
-     <Link activeClass="active" to="contactos" spy={true} smooth={true} offset={50} duration={500} className={s.contact}>Contactos</Link>
+      <Link
+        activeClass="active"
+        to="home"
+        spy={true}
+        smooth={true}
+        offset={0}
+        duration={500}
+        className={s.links}
+      >
+        Inicio
+      </Link>
+      <Link
+        activeClass="active"
+        to="sobremi"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className={s.links}
+      >
+        Sobre mi
+      </Link>
+      <Link
+        activeClass="active"
+        to="proyectos"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className={s.links}
+      >
+        Proyectos
+      </Link>
+      <a
+        href="mailto:correakevinfabian01@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={s.contact}
+      >
+        Contactame
+      </a>
     </div>
- )
+  );
 }
