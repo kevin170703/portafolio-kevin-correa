f74d5f97a4180f4d3049b4ee3a91f8a72cea968b
kevin170703 <correakevinfabian01@gmail.com>
2023-01-08 13:00:16 -0300

    remaster home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import s from "./Home.module.css";
 import profile from "../../img/profile.png";
+import { FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -13,6 +15,35 @@ export default function Home() {
           <h1>Kevin Correa</h1>
           <br />
           <h2>FullStack Developer</h2>
+          <div className={s.contentIcons}>
+            <a
+              href="https://www.linkedin.com/in/kevin-correa-dev/"
+              target="_BLANK"
+            >
+              <FaLinkedin size="40" className={s.icons} />
+            </a>
+            <a
+              href="mailto:correakevinfabian01@gmail.com"
+              target="_BLANK"
+              rel="noopener noreferrer"
+            >
+              <MdEmail size="50" className={s.icons} />
+            </a>
+            <a
+              href="https://wa.me/+543754506364"
+              target="_BLANK"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsapp size="40" className={s.icons} />
+            </a>
+          </div>
+          <a
+            href="mailto:correakevinfabian01@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className={s.contactame}>Contactame</button>
+          </a>
         </div>
       </div>
     </div>
