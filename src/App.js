de1ee89a2695373a1689e884dd9d92bb3fd15577
kevin170703 <correakevinfabian01@gmail.com>
2023-01-08 13:01:41 -0300

    extras

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import './App.css';
-import Nav from './components/NavBar/NavBar';
-import Home from './components/Home/Home';
-import SobreMi from './components/SobreMi/SobreMi';
-import Proyectos from './components/Proyectos/Proyectos';
-import Contactos from './components/Contactos/Contactos';
+import "./App.css";
+import Nav from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import SobreMi from "./components/SobreMi/SobreMi";
+import Proyectos from "./components/Proyectos/Proyectos";
 
 function App() {
   return (
     <div className="App">
-    <Nav/>
-    <Home/>
-    <SobreMi/>
-    <Proyectos/>
-    <Contactos/>
-    </div>    
+      <Nav />
+      <Home />
+      <SobreMi />
+      <Proyectos />
+    </div>
   );
 }
 
