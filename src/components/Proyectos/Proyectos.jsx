cd8c15bae0f614c5a866eb026486aa52360334d3
kevin170703 <correakevinfabian01@gmail.com>
2023-01-23 16:37:01 -0300

    rediseño de hover

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import s from "./Proyectos.module.css";
 import { useState, useRef } from "react";
-import { GrShop } from "react-icons/gr";
-import { TbPokeball } from "react-icons/tb";
-import { TiWeatherPartlySunny } from "react-icons/ti";
-import { MdWorkOutline } from "react-icons/md";
 import fondoServices from "../../img/services.png";
 import fondoPokemons from "../../img/pokemons.png";
 import fondoClima from "../../img/clima.png";
 import fondoVelvet from "../../img/velvet.png";
 import { useEffect } from "react";
+// icons
+import { DiJavascript1, DiReact, DiCss3, DiNodejsSmall } from "react-icons/di";
+import { GrFormNext } from "react-icons/gr";
+import {
+  SiSequelize,
+  SiRedux,
+  SiTypescript,
+  SiTailwindcss,
+  SiPostgresql,
+} from "react-icons/si";
 
 export default function Proyectos() {
   const [show, setShow] = useState(false);
-  const elemento = useRef();
+  const element = useRef();
   useEffect(() => {
     function handelView() {
-      const { y } = elemento.current.getBoundingClientRect();
+      const { y } = element.current.getBoundingClientRect();
       const res = y <= 400 ? true : false;
       setShow(res);
     }
@@ -24,97 +30,219 @@ export default function Proyectos() {
   }, []);
 
   return (
-    <div className={s.content} id="proyectos" ref={elemento}>
+    <div className={s.content} id="proyectos" ref={element}>
       <div className={s.contentImg}>
         <h2 className={s.title}>Proyectos:</h2>
         <a href="https://services-jobs.vercel.app" target="_blank">
-          <div className={show === false ? s.contentNo : s.proyectos}>
+          <div className={show === false ? s.notVisible : s.proyects}>
             <img src={fondoServices} alt="" />
 
             {/* <div className={s.logo}>
               <MdWorkOutline size="50" className={s.logos} />
             </div> */}
             <div className={s.info}>
-              <h5 className={s.title_proyectos}>App services</h5>
+              <h5 className={s.title_proyects}>App services</h5>
               <p>
-                Una una página donde pueden encontrar y publicar servicios de
-                forma rapida y sencilla.
+                Una una página web donde pueden encontrar y publicar servicios
+                de forma rapida y sencilla.
               </p>
-              <p>
-                Tecnologias utilizadas:
-                <br />
-                <br /> React, Redux
-                <br /> Node.js, Express
-                <br /> PostgresSQL, Sequelize
-                <br /> JavaScript, HTML, CSS
-              </p>
+              <div className={s.contentIcons}>
+                <p className={s.titleTencologies}>Tecnologias utilizadas:</p>
+                <div className={s.contentLogos}>
+                  <DiJavascript1
+                    size="30"
+                    className={s.imagenes}
+                    color="#ead41c"
+                  />
+                  <p>JavaSript</p>
+                </div>
+
+                <div className={s.contentLogos}>
+                  <DiReact size="30" className={s.imagenes} color="#5ccfee" />
+                  <p>React</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiRedux size="30" className={s.imagenes} color="#7046b2" />
+                  <p>Redux</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <DiNodejsSmall
+                    size="30"
+                    className={s.imagenes}
+                    color="#7cc327"
+                  />
+                  <p>Node.js</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiPostgresql
+                    size="30"
+                    className={s.imagenes}
+                    color="#03a6e3"
+                  />
+                  <p>PostgreSQL</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiSequelize
+                    size="30"
+                    className={s.imagenes}
+                    color="#03a6e3"
+                  />
+                  <p>Sequelize</p>
+                </div>
+              </div>
             </div>
           </div>
         </a>
         <a href="https://velvetpf.vercel.app/" target="_blank">
-          <div className={show === false ? s.contentNo : s.proyectos}>
+          <div className={show === false ? s.notVisible : s.proyects}>
             <img src={fondoVelvet} alt="" />
             {/* <div className={s.logo}>
               <GrShop size="50" className={s.logos} />
             </div> */}
             <div className={s.info}>
-              <h5 className={s.title_proyectos}>Velvet ecommerce</h5>
+              <h5 className={s.title_proyects}>Velvet ecommerce</h5>
               <p>Un ecommerce donde pudes encontrat tus prendas favoritas.</p>
-              <p>
-                Tecnologias utilizadas:
-                <br />
-                <br /> React, Redux, Tailwindcss
-                <br /> Node.js, Express
-                <br /> PostgresSQL, Sequelize
-                <br /> JavaScript, HTML, CSS
-              </p>
+              <div className={s.contentIcons}>
+                <p className={s.titleTencologies}>Tecnologias utilizadas:</p>
+                <div className={s.contentLogos}>
+                  <DiJavascript1
+                    size="30"
+                    className={s.imagenes}
+                    color="#ead41c"
+                  />
+                  <p>JavaSript</p>
+                </div>
+
+                <div className={s.contentLogos}>
+                  <DiReact size="30" className={s.imagenes} color="#5ccfee" />
+                  <p>React</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiRedux size="30" className={s.imagenes} color="#7046b2" />
+                  <p>Redux</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <DiNodejsSmall
+                    size="30"
+                    className={s.imagenes}
+                    color="#7cc327"
+                  />
+                  <p>Node.js</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiSequelize
+                    size="30"
+                    className={s.imagenes}
+                    color="#03a6e3"
+                  />
+                  <p>Sequelize</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiTailwindcss
+                    size="30"
+                    className={s.imagenes}
+                    color="#07adca"
+                  />
+                  <p>Tailwindcss</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <GrFormNext
+                    size="30"
+                    className={s.imagenes}
+                    color="#07adca"
+                  />
+                  <p>Entre otras...</p>
+                </div>
+              </div>
             </div>
           </div>
         </a>
         <a href="https://pokemon-app-kevin170703.vercel.app/" target="_blank">
-          <div className={show === false ? s.contentNo : s.proyectos}>
+          <div className={show === false ? s.notVisible : s.proyects}>
             <img src={fondoPokemons} alt="" />
 
             {/* <div className={s.logo}>
               <TbPokeball size="70" className={s.logos} />
             </div> */}
             <div className={s.info}>
-              <h5 className={s.title_proyectos}>App pokemons</h5>
+              <h5 className={s.title_proyects}>App pokemons</h5>
               <p>
                 Un pagina donde podras ver, crear y buscar tus pokemones
                 favotitos.
               </p>
-              <p>
-                Tecnologias utilizadas:
-                <br />
-                <br /> React, Redux
-                <br /> Node.js, Express
-                <br /> PostgresSQL, Sequelize
-                <br /> JavaScript, HTML, CSS
-              </p>
+              <div className={s.contentIcons}>
+                <p className={s.titleTencologies}>Tecnologias utilizadas:</p>
+                <div className={s.contentLogos}>
+                  <DiJavascript1
+                    size="30"
+                    className={s.imagenes}
+                    color="#ead41c"
+                  />
+                  <p>JavaSript</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <DiReact size="30" className={s.imagenes} color="#5ccfee" />
+                  <p>React</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiRedux size="30" className={s.imagenes} color="#7046b2" />
+                  <p>Redux</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <DiNodejsSmall
+                    size="30"
+                    className={s.imagenes}
+                    color="#7cc327"
+                  />
+                  <p>Node.js</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiPostgresql
+                    size="30"
+                    className={s.imagenes}
+                    color="#03a6e3"
+                  />
+                  <p>PostgreSQL</p>
+                </div>
+                <div className={s.contentLogos}>
+                  <SiSequelize
+                    size="30"
+                    className={s.imagenes}
+                    color="#03a6e3"
+                  />
+                  <p>Sequelize</p>
+                </div>
+              </div>
             </div>
           </div>
         </a>
         <a href="https://app-clima-correa-kevin.netlify.app/" target="_blank">
-          <div className={show === false ? s.contentNo : s.proyectos}>
+          <div className={show === false ? s.notVisible : s.proyects}>
             <img src={fondoClima} alt="" />
 
             {/* <div className={s.logo}>
               <TiWeatherPartlySunny size="50" className={s.logos} />
             </div> */}
             <div className={s.info}>
-              <h5 className={s.title_proyectos}>App clima</h5>
+              <h5 className={s.title_proyects}>App clima</h5>
               <p>Un lugar donde podras ver el cima de cuidades y paises.</p>
 
-              <p>
-                {" "}
-                Tecnologias utilizadas:
-                <br />
-                <br /> React
-                <br /> JavaScript
-                <br /> HTML
-                <br /> CSS
-              </p>
+              <div className={s.contentIcons}>
+                <p className={s.titleTencologies}>Tecnologias utilizadas:</p>
+                <div className={s.contentLogos}>
+                  <DiJavascript1
+                    size="30"
+                    className={s.imagenes}
+                    color="#ead41c"
+                  />
+                  <p>JavaSript</p>
+                </div>
+
+                <div className={s.contentLogos}>
+                  <DiReact size="30" className={s.imagenes} color="#5ccfee" />
+                  <p>React</p>
+                </div>
+              </div>
             </div>
           </div>
         </a>
